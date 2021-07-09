28eefe1aa1ab271c00071293dec509db56b79d9b
Swetasmita <schinray@gmail.com>
2021-07-09 23:43:12 +0100

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import './style.css' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const price = 2000;  //Variable and object values
+const tax  = price * 0.10; //Expression & operator
+const car = "MG Hector"; //Function calla & reference
+const specifications ={
+length: 4655,
+width : 1835,
+height : 1760
+}
+const bookprice = 300;
+
+const getDimensions = specifications =>(
+  `${specifications.length}(mm) ${specifications.width}(mm) ${specifications.height}(mm)`
+);
+var myStyle ={
+  fontSize: 30,
+  color: '#FF0000',
+ 
 }
 
-export default App;
+function App() {
+
+  //Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  const[isBlue, setBlue]= useState(false);
+
+  const change = () =>{
+    setCount(count+1)
+    setBlue(true);
+  }
+  return (
+    <div className="app">
+    <h1>My React App!</h1>
+    <h2 style ={myStyle}>Welcome to my Page</h2>
+   
+  <h3 className={isBlue ?'blue': "" }>Change My Color</h3>
+   {/*Increment Counter */}
+  <h4>You clicked {count} times</h4>
+  <button onClick ={change}>
+   Click Me
+ </button>
+
+   <div class="container">
+    <button onClick={() => alert("hello")}>alert hello</button>
+    <br/> <br/>
+    Car Price : {price + tax}
+  <br/>
+  <p>{car}</p>
+  <p>{getDimensions(specifications)}</p>
+  const cars = ["MG Hector", "Hyundai Venue", "Toyota Fortuner",
+			  "Hyundai Creta"];
+       </div>
+       </div>
+    );
+    <br/>
+ 
+}
+export default App; //Only one deafault app is allowed
+
